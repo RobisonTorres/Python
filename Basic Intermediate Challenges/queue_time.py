77f658587259a11899cf58e2cae2aa047ec06920
Robison Torres <robison.torres21@gmail.com>
2022-09-22 14:09:13 -0300

    Initial commit.

--- Basic Intermediate Challenges/queue_time.py
+++ Basic Intermediate Challenges/queue_time.py
@@ -1,13 +1,3 @@
 print('From Code Wars.')
 print()
 
-def queue_time(customers, n):
-
-    # This function is boring.
-    if customers:
-        return sum(customers)/ n if len(customers) > n else max(customers)
-    else:
-        return 0
-
-print(queue_time([1,2,3,4,5], 100))
-print(queue_time([], 1))
