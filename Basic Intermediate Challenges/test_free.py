2c72e9e74e0ad66ccc4d29809893b55d703f09f1
Robison Torres <robison.torres21@gmail.com>
2022-09-26 11:23:37 -0300

    Initial commit.

--- Basic Intermediate Challenges/test_free.py
+++ Basic Intermediate Challenges/test_free.py
@@ -1,15 +1,3 @@
 print('Test Free - keep it clean')
 print()
 
-def queue_time(customers, n):
-
-    total_minutes = 0
-    while customers:
-        sub = min(customers[0: n])
-        total_minutes += sub
-        minutes = [num - sub for num in customers[0:n]]
-        customers[0: n] = minutes
-        customers = [num for num in customers if num != 0]
-    return total_minutes
-
-print(queue_time([4, 8, 5, 2, 4], 1))
