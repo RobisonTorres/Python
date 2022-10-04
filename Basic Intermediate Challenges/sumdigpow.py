84313b84faf8a40566ca4cd5f1f810c897ab90f1
Robison Torres <robison.torres21@gmail.com>
2022-10-04 12:08:13 -0300

    Initial commit.

--- Basic Intermediate Challenges/sumdigpow.py
+++ Basic Intermediate Challenges/sumdigpow.py
@@ -1,16 +1,8 @@
-print('Test Free - keep it clean')
+print('From Code Wars.')
 print()
 
 def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
 
-    '''
-    In effect: 89 = 8^1 + 9^2
-    The next number in having this property is 135.
-    See this property again: 135 = 1^1 + 3^2 + 5^3
-    We need a function to collect these numbers, that may receive two integers 
-    a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted
-    numbers in the range that fulfills the property described above.
-    '''
     numbers = []
     for x in range(a, b+1):
         n = [int(y)+d for y in str(x) for d in range(1, len(y)+1)]
