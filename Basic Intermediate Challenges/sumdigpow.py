2288a987f5d42ebf3d466800ec00babc9d703c58
Robison Torres <robison.torres21@gmail.com>
2022-10-05 10:25:17 -0300

    Initial commit.

--- Basic Intermediate Challenges/sumdigpow.py
+++ Basic Intermediate Challenges/sumdigpow.py
@@ -1,9 +1,9 @@
 print('From Code Wars.')
 print()
 
-def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
+def sum_dig_pow(a, b):
 
-    # Property: 135 = 1^1 + 3^2 + 5^3
+    # Property: 89 = 8^1 + 9^2, 135 = 1^1 + 3^2 + 5^3
     # Given a range of numbers (a, b), 
     # this function collects numbers that fulfills the property described above.
     numbers = []
@@ -13,8 +13,8 @@ def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
         while d <= len(str(x)):
             n += int(str(x)[-1+d])**d
             d += 1
-            if x == n:
-                numbers.append(n)
+        if x == n:
+            numbers.append(x)
     return numbers
 
 print(sum_dig_pow(1, 1000))  # Outputs - [1, 2, 3, 4, 5, 6, 7, 8, 9, 89, 135, 175, 518, 598]
--- Basic Intermediate Challenges/test_free.py
+++ Basic Intermediate Challenges/test_free.py
@@ -1,20 +1,3 @@
 print('Test Free - keep it clean')
 print()
 
-def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
-
-    # Property: 135 = 1^1 + 3^2 + 5^3
-    # Given a range of numbers (a, b), 
-    # this function collects numbers that fulfills the property described above.
-    numbers = []
-    for x in range(a, b+1):
-        d = 1
-        n = 0
-        while d <= len(str(x)):
-            n += int(str(x)[-1+d])**d
-            d += 1
-            if x == n:
-                numbers.append(n)
-    return numbers
-
-print(sum_dig_pow(1, 1000))  # Outputs - [1, 2, 3, 4, 5, 6, 7, 8, 9, 89, 135, 175, 518, 598]
