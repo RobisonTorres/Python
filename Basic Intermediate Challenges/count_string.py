90e5c45b008d86da2bf094a515fb2e8a896de8be
Robison Torres <robison.torres21@gmail.com>
2022-10-19 10:43:28 -0300

    Initial commit.

--- Basic Intermediate Challenges/count_string.py
+++ Basic Intermediate Challenges/count_string.py
@@ -4,9 +4,13 @@ print()
 def count(string):
     
     # This function counts all the occurring characters in a string.
-    string = ''.join(string.lower().split(' '))
-    keys = ''.join(sorted(set(string)))
-    occurrence = [string.count(l) for l in keys]
-    return dict(zip(keys, occurrence))
+    string = string.replace(' ', '')
+    occurrence = [string.count(l) for l in string]
+    return dict(zip(string, occurrence))
+    ''' - Clever
+    from collections import Counter
+    def count(string):
+        return Counter(string)
+    ''' 
 
-print(count('Download Movies'))
+print(count('Download'))  # {'D': 1, 'o': 2, 'w': 1, 'n': 1, 'l': 1, 'a': 1, 'd': 1}
