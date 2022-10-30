56b5a2e476c873490d283ae124e8868f9ef08f63
Robison Torres <robison.torres21@gmail.com>
2022-10-30 16:54:39 -0300

    Initial commit.

---
+++ Basic Intermediate Challenges/roman_numerals_decoder.py
@@ -0,0 +1,18 @@
+print('From Code Wars.')
+print()
+
+def solution(roman):
+    
+    # This function takes a roman numeral representation (I - MMMCMXCIX), 
+    # and returns its values as a numeric integer.
+    roman_numbers = {
+
+        'M': 1000, 'C': 100, 'D': 500, 'X': 10, 'L': 50, 'I': 1, 'V': 5
+    }
+
+    numbers = [roman_numbers[n] for n in [l for l in roman]]
+    new_number = [x[0] * (-1) if x[0] < x [1] else x[0] 
+                    for x in zip(numbers, numbers[1:] + [0])]
+    return sum(new_number)
+
+print(solution('MCMXC'))  # Outputs - 1990
