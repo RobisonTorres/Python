b1e30efc1e57bda5e9014617ae16352a6494c362
Robison Torres <robison.torres21@gmail.com>
2022-10-30 11:18:47 -0300

    Initial commit.

--- Basic Intermediate Challenges/test_free.py
+++ Basic Intermediate Challenges/test_free.py
@@ -1,14 +1,3 @@
 print('Test Free - keep it clean')
 print()
 
-def to_weird_case(str):
-    
-    # This function...
-    list_letters = []
-    string = str.split()
-    for i in range(len(string)):
-         list_letters.append(''.join([l.upper() if n % 2 == 0  else l.lower() 
-                            for n, l in enumerate(string[i])]))
-    return ' '.join(list_letters)
-
-print(to_weird_case('Weird string case'))  # Outputs - WeIrD StRiNg CaSe
--- Basic Intermediate Challenges/weird_case.py
+++ Basic Intermediate Challenges/weird_case.py
@@ -1,3 +1,17 @@
 print('From Code Wars.')
 print()
 
+def to_weird_case(str):
+    
+    # This function takes a string and returns the same string with all even indexed
+    # characters in each word upper cased, and all odd indexed characters in lower cased.
+    list_letters = []
+    string = str.split()
+    for i in range(len(string)):
+         list_letters.append(''.join([l.upper() if n % 2 == 0  else l.lower() 
+                            for n, l in enumerate(string[i])]))
+    return ' '.join(list_letters)
+    # return ' '.join([''.join([y.lower() if i%2 else y.upper() for i, y in enumerate(x)])
+    # for x in string.split()]) - Clever.
+
+print(to_weird_case('Weird string case'))  # Outputs - WeIrD StRiNg CaSe
