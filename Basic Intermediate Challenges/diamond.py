ba00f516de60d28f75be0fe0153d3eb9b7bbb295
Robison Torres <robison.torres21@gmail.com>
2022-11-04 16:28:06 -0300

    Initial Commit.

--- Basic Intermediate Challenges/diamond.py
+++ Basic Intermediate Challenges/diamond.py
@@ -3,16 +3,17 @@ print()
 
 def diamond(n):
 
-    # Make some diamonds!
-    if n == '':
+    ''' You need to return a string that looks like a diamond shape when printed
+    on the screen, using asterisk (*) characters. Trailing spaces should be removed,
+    and every line must be terminated with a newline character (\n).'''
+
+    if n == '' or n <= 0 or n % 2 == 0:
         return None
-    else:
-        new = []
-        x = 1
-        while x <= n:
-            new.append((("*" * x).center(n)).rstrip() + '\\n')
-            x += 2
-        result = ''.join(new + new[0: len(new) - 1][::-1])
-        return None if n % 2 == 0 or n <= 0 else result
+    new = []
+    x = 1
+    while x <= n:
+        new.append((("*" * x).center(n)).rstrip() + '\\n')
+        x += 2
+    return ''.join(new + new[0: len(new) - 1][::-1])
 
 print(diamond(3))  # Outputs -  *\n***\n *\n
