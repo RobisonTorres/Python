f8fd854a0841508ffa2730f46104461742177401
Robison Torres <robison.torres21@gmail.com>
2022-11-07 15:16:15 -0300

    Initial commit.

--- Basic Intermediate Challenges/parse.py
+++ Basic Intermediate Challenges/parse.py
@@ -3,17 +3,16 @@ print()
 
 def parse(text):
 
+    # Write a simple parser that will parse and run Deadfish.
+    # It has 4 commands, each 1 character long:
+    # 'i': + 1, 'd': - 1, 's': ** 2, 'o': append()
     x = 0
     n = []
     for y in range(len(text)):
-        if text[y] == 'i':
-            x += 1
-        if text[y] == 'd':
-            x -= 1
-        if text[y] == 's':
-            x **= 2
-        if text[y] == 'o':
-            n.append(x)
+        if text[y] == 'i': x += 1
+        if text[y] == 'd': x -= 1
+        if text[y] == 's': x **= 2
+        if text[y] == 'o': n.append(x)
     return n
 
-print(parse("faosdfads"))
+print(parse("idiiidiisoso"))  # Outputs - [16, 256]
