ada28820193465db312a0aaf42c650294a139608
Robison Torres <robison.torres21@gmail.com>
2022-11-08 11:34:07 -0300

    Initial commit.

--- Basic Intermediate Challenges/test_free.py
+++ Basic Intermediate Challenges/test_free.py
@@ -1,3 +1,6 @@
 print('Test Free - keep it clean')
 print()
 
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
+list_names = s.split(';')
+print(list_names)
