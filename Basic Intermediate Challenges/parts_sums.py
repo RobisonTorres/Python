ab3fd179c85c565c5ec95f0ab3116606a0455f9f
Robison Torres <robison.torres21@gmail.com>
2022-11-15 14:10:52 -0300

    Initial commit.

--- Basic Intermediate Challenges/parts_sums.py
+++ Basic Intermediate Challenges/parts_sums.py
@@ -4,7 +4,7 @@ print()
 def parts_sums(ls):
     
     '''
-    This function sums parts of array reducing the size of it in each iteration, 
+    This function sums parts of an array, reducing the size of it in each iteration, 
     and puts the result in a new list.
     ls = [0, 1, 3, 6, 10], ls = [1, 3, 6, 10], ls = [3, 6, 10], ls = [6, 10], ls = [10], ls =  []
     '''
@@ -12,4 +12,5 @@ def parts_sums(ls):
     [result.append(result[-1] - ls[i]) for i in range(0, len(ls))]
     return result
 
-print(parts_sums([0, 1, 3, 6, 10]))  # Outputs - [20, 20, 19, 16, 10, 0]
+print(parts_sums([0, 1, 3, 6, 10]))  
+# Outputs - [20, 20, 19, 16, 10, 0]
