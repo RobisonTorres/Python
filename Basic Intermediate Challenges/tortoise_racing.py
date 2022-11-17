28ca0aed1d8dcc6df22488e822c82766ad35f40e
Robison Torres <robison.torres21@gmail.com>
2022-11-17 16:06:05 -0300

    Initial commit.

---
+++ Basic Intermediate Challenges/tortoise_racing.py
@@ -0,0 +1,13 @@
+print('From Code Wars.')
+print()
+
+def race(v1, v2, g):
+    
+    # Given two speeds v1 (A) and v2 (B) and a lead g,
+    # how long will it take to B catch A? 
+    time = (g / (v2 - v1)) + 0.000001
+    t_time = [int(time), (time - int(time)) * 60]
+    t_time.append((t_time[1] - int(t_time[1])) * 60)
+    return None if v1 >= v2 else list(map(int, t_time))
+
+print(race(80, 91, 37))  # Outputs - 3h21min49s - To A catch B.
