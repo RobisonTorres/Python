0f7c128f8ac6043d6178fbfcbb08e3310fa096d7
Robison Torres <robison.torres21@gmail.com>
2022-11-19 18:48:03 -0300

    Initial commit.

--- Intermediate Challenges/test_free.py
+++ Intermediate Challenges/test_free.py
@@ -1,6 +1,6 @@
 import sys, subprocess
 subprocess.run('cls', shell=True)
-print('Test Free - keep it clear')
+print('Test Free - Inter. Challenges - 100')
 print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 
