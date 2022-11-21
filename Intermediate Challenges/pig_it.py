6ee66cf2cc37efc61961b789b63d44d847e2c12d
Robison Torres <robison.torres21@gmail.com>
2022-11-21 11:21:21 -0300

    Initial commit.

---
+++ Intermediate Challenges/pig_it.py
@@ -0,0 +1,10 @@
+print('From Code Wars.')
+print()
+
+def pig_it(string):
+
+    # This function moves the first letter of each word to the end of it,
+    # then add "ay" to the end of the word. Leave punctuation marks untouched.
+    return ' '.join([w[1:] + w[0] + 'ay' if w.isalpha() else w for w in string.split()])
+
+print(pig_it('Pig latin is cool !')) # Output - igPay atinlay siay oolcay ! 
