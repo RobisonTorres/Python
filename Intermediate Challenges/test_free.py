6a160d9c3dc1b791abc40fb7a7fcec397fffbeac
Robison Torres <robison.torres21@gmail.com>
2022-11-26 14:16:55 -0300

    Initial commit.

--- Intermediate Challenges/test_free.py
+++ Intermediate Challenges/test_free.py
@@ -5,3 +5,9 @@ print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
 
+def max_sequence(arr):
+    
+    # This function...
+    return arr
+
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
