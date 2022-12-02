b6545a6292299e6cca01c8ac80861aa5148cd7e2
Robison Torres <robison.torres21@gmail.com>
2022-12-02 10:17:29 -0300

    Initial commit.

--- Basic Intermediate Challenges/roman_numerals_encoder.py
+++ Basic Intermediate Challenges/roman_numerals_encoder.py
@@ -13,14 +13,16 @@ def solution():
         num = num[1:]
     numbers = [int(n) for n in nums if not n.startswith('0')]
 
-    dict_M = {1000: 'M', 2000: 'MM', 3000: 'MMM', 40000: 'IV',5000: 'V', 6000: 'VI',
-              7000: 'VII', 8000: 'VIII', 9000: 'IX', 
-              100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC',
-              700: 'DCC', 800: 'DCCC', 900: 'CM', 
-              10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX',
-              70: 'LXX', 80: 'LXXX', 90: 'XC', 
-              1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI',
-              7: 'VII', 8: 'VIII', 9: 'IX'}
+    dict_M = {
+        1000: 'M', 2000: 'MM', 3000: 'MMM', 4000: 'IV',5000: 'V', 6000: 'VI',
+        7000: 'VII', 8000: 'VIII', 9000: 'IX', 
+        100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC',
+        700: 'DCC', 800: 'DCCC', 900: 'CM', 
+        10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX',
+        70: 'LXX', 80: 'LXXX', 90: 'XC', 
+        1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI',
+        7: 'VII', 8: 'VIII', 9: 'IX'
+        }
     
     return 'The result is '+''.join([dict_M[r] for r in numbers]) +'.'
 
