53fc990a0050f5446750e19adad24e50cee86307
Robison Torres <robison.torres21@gmail.com>
2022-12-04 11:05:19 -0300

    Initial commit.

--- IntermediateChallenges_5kyu/bake_cake.py
+++ IntermediateChallenges_5kyu/bake_cake.py
@@ -2,4 +2,16 @@ print('From Code Wars.')
 print()
 
 def cakes(recipe, available):
-    return 5
+    
+    # Task - Write a function which takes the recipe and the available ingredients,
+    # and returns the maximum number of cakes that can be made.
+    try:
+        result = []
+        for key, value in recipe.items():
+            result.append(int(available[key]/ value))
+        return min(result)
+    except: return 0
+
+recipe = {"flour": 500, "sugar": 200, "eggs": 1, "milk": 10}
+available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+print(cakes(recipe, available))  # Outputs - 2 cakes
