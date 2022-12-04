60582d956ac58e7447d4b349ff3a1da4da886e6e
Robison Torres <robison.torres21@gmail.com>
2022-12-04 09:48:08 -0300

    Initial commit.

---
+++ IntermediateChallenges_5kyu/bake_cake.py
@@ -0,0 +1,5 @@
+print('From Code Wars.')
+print()
+
+def cakes(recipe, available):
+    return 5
--- test_free.py
+++ test_free.py
@@ -5,3 +5,16 @@ print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
 
+def cakes(recipe, available):
+    
+    # This function...
+    return [list(recipe.keys()), list(available.keys())]
+    '''
+    if recipe.keys() in available.keys() and all([n > 0 for n in available.values()]):
+        return "bake some cakes"
+    else:
+        return 0
+    '''
+recipe = {"flour": 500, "sugar": 200, "eggs": 1, "milk": 10}
+available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+print(cakes(recipe, available))
