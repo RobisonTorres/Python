872f3bea2dee27620d81965fc0db8497ff38a9d3
Robison Torres <robison.torres21@gmail.com>
2022-12-06 13:29:25 -0300

    Initial commit.

--- IntermediateChallenges_5kyu/domain_name.py
+++ IntermediateChallenges_5kyu/domain_name.py
@@ -1,14 +1,17 @@
+import re
 print('From Code Wars')
 print()
 
 def domain_name(url):
-    
-    # This function extract the url domain's name of a web site.
-    url = url.replace('www.','')
-    url = url.replace('https://','')
-    url = url.replace('http://','')
-    
-    return url[0:url.find('.')]
+
+    match = re.findall(r'(?:http(?:s?)://)?(?:www.)?([\w-]+)', url)
+    return match[0]
     # return re.search('(https?://)?(www\d?\.)?(?P<name>[\w-]+)\.', url).group('name') - Clever.
 
-print(domain_name('http://www.zombie-bites.com'))  # Outputs - zombie-bites
+print(domain_name('http://www.zombie-bites.br.com'))  # Outputs - zombie-bites
+
+'''
+?: - It indicates that the subpattern is a non-capture subpattern.
+That means whatever is matched in (?:\w+\s) , even though it's enclosed
+by () it won't appear in the list of matches, only (\w+) will.
+'''
