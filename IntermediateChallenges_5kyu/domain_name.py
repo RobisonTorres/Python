46dec867f04039ef925b8eced13086483f23b0bd
Robison Torres <robison.torres21@gmail.com>
2022-12-06 11:48:34 -0300

    Initial commit.

--- IntermediateChallenges_5kyu/domain_name.py
+++ IntermediateChallenges_5kyu/domain_name.py
@@ -1,3 +1,14 @@
 print('From Code Wars')
 print()
 
+def domain_name(url):
+    
+    # This function extract the url domain's name of a web site.
+    url = url.replace('www.','')
+    url = url.replace('https://','')
+    url = url.replace('http://','')
+    
+    return url[0:url.find('.')]
+    # return re.search('(https?://)?(www\d?\.)?(?P<name>[\w-]+)\.', url).group('name') - Clever.
+
+print(domain_name('http://www.zombie-bites.com'))  # Outputs - zombie-bites
