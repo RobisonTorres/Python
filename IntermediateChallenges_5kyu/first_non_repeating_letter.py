fcd63425a10ebacb2df975411939431021a2d5a3
Robison Torres <robison.torres21@gmail.com>
2022-12-06 10:00:14 -0300

    Initial commit.

---
+++ IntermediateChallenges_5kyu/first_non_repeating_letter.py
@@ -0,0 +1,18 @@
+print('From Code Wars.')
+print()
+
+def first_non_repeating_letter(string):
+
+    # Task - Write a function that takes a string, and returns 
+    # the first character that is not repeated anywhere in the string.
+    copy = string.lower()
+    copy_count = ''.join([l for l in copy if copy.count(l) == 1])
+    if copy == '' or copy_count == '':
+        return ''
+    else:
+        try:
+            return string[string.index(copy_count[0])]
+        except:
+            return string[string.index(copy_count[0].upper())]
+
+print(first_non_repeating_letter('Toshiba'))
--- test_free.py
+++ test_free.py
@@ -1,6 +1,6 @@
 import sys, subprocess
 subprocess.run('cls', shell=True)
-print('Test Free - Inter. Challenges - 100')
+print('Test Free')
 print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
