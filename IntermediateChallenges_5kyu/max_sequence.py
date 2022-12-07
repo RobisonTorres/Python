2ff7d1c3fbb518cd06f6ed9736cb40a03183bbc3
Robison Torres <robison.torres21@gmail.com>
2022-12-07 12:44:20 -0300

    Initial commit.

---
+++ IntermediateChallenges_5kyu/max_sequence.py
@@ -0,0 +1,10 @@
+print('From Code Wars.')
+print()
+
+def max_sequence(arr):
+
+    # Task - The maximum sum sub array problem consists in finding the maximum 
+    # sum of a contiguous subsequence in an array or list of integers.
+    pass
+
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
--- test_free.py
+++ test_free.py
@@ -5,3 +5,8 @@ print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
 
+def max_sequence(arr):
+
+    pass
+
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
