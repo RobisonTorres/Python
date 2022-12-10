1d259df64cd836c86a5e51cf768b6db5af009466
Robison Torres <robison.torres21@gmail.com>
2022-12-10 12:01:24 -0300

    Work finished.

--- Projects/Youtube_Downloader/youtube_downloader.py
+++ Projects/Youtube_Downloader/youtube_downloader.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 import sys, subprocess
 subprocess.run('cls', shell=True)
 print('Youtube Downloader.')
@@ -8,7 +9,7 @@ def download():
 
     # This function downloads youtube videos.
     link = input("Please, type in the videos's url you want to download... ")
-    youtube_video = YouTube(link)
+    youtube_video = YouTube(link, on_progress_callback=on_progress)
     youtube_video = youtube_video.streams.get_highest_resolution()
     try:
         youtube_video.download(r'C:\Users\Robison Torres\Downloads')  # Chose the directory.
