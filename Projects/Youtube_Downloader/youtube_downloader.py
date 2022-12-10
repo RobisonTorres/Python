4e4ae28e73f9f736faf84b4fcf3addf1e32d9d9a
Robison Torres <robison.torres21@gmail.com>
2022-12-10 18:45:38 -0300

    Work finished.

--- Projects/Youtube_Downloader/youtube_downloader.py
+++ Projects/Youtube_Downloader/youtube_downloader.py
@@ -1,4 +1,4 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 from pytube.cli import on_progress
 import sys, subprocess
 subprocess.run('cls', shell=True)
@@ -8,12 +8,33 @@ print()
 def download():
 
     # This function downloads youtube videos.
-    link = input("Please, type in the videos's url you want to download... ")
-    youtube_video = YouTube(link, on_progress_callback=on_progress)
-    youtube_video = youtube_video.streams.get_highest_resolution()
-    try:
-        youtube_video.download(r'C:\Users\Robison Torres\Downloads')  # Chose the directory.
-        return 'Download successfully.'
-    except: return 'Sorry, something went wrong.'
+
+    option = int(input('Press 1 for Video or Press 2 for Playlist: '))
+    directory = (input(r"Type in the directory's path or hit enter to save it on default directory (Downloads): ")
+                or r'C:\Users\Robison Torres\Downloads')
+
+    if option == 1:
+        link = input("Type in the videos's url you want to download: ")
+        youtube_video = YouTube(link, on_progress_callback=on_progress)
+        youtube_video = youtube_video.streams.get_highest_resolution()
+        try:
+            youtube_video.download(directory)
+            return 'Download successfully.'
+        except: return 'Sorry, something went wrong.'
+
+    elif option == 2:
+        link = input("Type in the playlist's url you want to download... ")
+        playlist = Playlist(link)
+        playlist_videos = list(playlist.video_urls)
+        try:
+            for video in playlist_videos:
+                youtube_video = YouTube(video, on_progress_callback=on_progress)
+                youtube_video = youtube_video.streams.get_highest_resolution()
+                youtube_video.download(directory)
+            return 'Download successfully.'
+        except: return 'Sorry, something went wrong.'
+    
+    else:
+        return 'Wrong option.'
 
 print(download())
