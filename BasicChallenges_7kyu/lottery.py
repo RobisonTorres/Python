5c4a5ad16f5174190069eabf3073dbbd7e4524ac
Robison Torres <robison.torres21@gmail.com>
2022-12-11 10:55:14 -0300

    Code upgrade.

--- BasicChallenges_7kyu/lottery.py
+++ BasicChallenges_7kyu/lottery.py
@@ -1,10 +1,11 @@
 print('Checking number...')
 print()
 
-def score(game, result):
+def score(lottery_numbers, your_numbers):
 
-    # This function checks the total score in each game.
-    return 15 - len(set(result) - set(game))
-
-print(score([numbers],
-            [result]))
+    # This function checks the total score hit in each lottery ticket.
+    result = [int(lottery_numbers[x: x+2]) for x in range(0, len(lottery_numbers), 2)]   
+    return 15 - len(set(result) - set(your_numbers))
+    
+print(score('', 
+            []))
