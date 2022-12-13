530ae45a641895c407823296291e84cf5158b6a4
Robison Torres <robison.torres21@gmail.com>
2022-12-13 11:05:34 -0300

    Project finished.

--- Projects/Youtube_Downloader/youtube_downloader.py
+++ Projects/Youtube_Downloader/youtube_downloader.py
@@ -9,37 +9,41 @@ print()
 def download():
 
     # This function downloads youtube videos.
-    option = int(input('Press 1 to download Video or Press 2 to download Playlist: '))
-    downloads_path = str(Path.home() / "Downloads")
-    print()
-    directory = (input(r"Download Location - Type in the directory's path or hit enter to choose the default directory (Downloads):" + '\n')
-                         or downloads_path)
-
-    if option == 1:
+    start = 'Yes'
+    while start == 'Yes':
+        option = int(input('Press 1 to download Video or Press 2 to download Playlist: '))
+        downloads_path = str(Path.home() / "Downloads")
         print()
-        link = input("Type in the videos's url that you want to download:\n")
-        youtube_video = YouTube(link, on_progress_callback=on_progress)
-        youtube_video = youtube_video.streams.get_highest_resolution()
-        try:
-            youtube_video.download(directory)
-            return 'Download successfully.'
-        except: return 'Sorry, something went wrong.'
+        directory = (input(r"Download Location - Type in the directory's path or hit enter to choose the Downloads folder:" + '\n')
+                            or downloads_path)
 
-    elif option == 2:
-        print()
-        link = input("Type in the playlist's url that you want to download:\n")
-        playlist = Playlist(link)
-        playlist_videos = list(playlist.video_urls)
-        try:
-            for num, video in enumerate(playlist_videos, 1):
-                youtube_video = YouTube(video)
-                youtube_video = youtube_video.streams.get_highest_resolution()
+        if option == 1:
+            print()
+            link = input("Type in the videos's url that you want to download:\n")
+            youtube_video = YouTube(link, on_progress_callback=on_progress)
+            youtube_video = youtube_video.streams.get_highest_resolution()
+            try:
                 youtube_video.download(directory)
-                print('Video ' + str(num) + ' is Downloaded.')
-            return 'Download successfully.'
-        except: return 'Sorry, something went wrong.'
+            except: return 'Sorry, something went wrong.'
+
+        elif option == 2:
+            print()
+            link = input("Type in the playlist's url that you want to download:\n")
+            playlist = Playlist(link)
+            playlist_videos = list(playlist.video_urls)
+            try:
+                for num, video in enumerate(playlist_videos, 1):
+                    youtube_video = YouTube(video)
+                    youtube_video = youtube_video.streams.get_highest_resolution()
+                    youtube_video.download(directory)
+                    print('Video ' + str(num) + ' is Downloaded.')
+            except: return 'Sorry, something went wrong.'
     
-    else:
-        return 'Wrong option.'
+        else:
+            return 'Wrong option.'
+        print()
+        start = input("Type in 'Yes' if you want to continue or anything else to stop:\n" ).capitalize()
+        subprocess.run('cls', shell=True)
+    return 'Download successfully.'
 
 print(download())
