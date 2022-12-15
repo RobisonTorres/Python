110e84606f979a10efa4f4ecb849e4e13a1b5e2b
Robison Torres <robison.torres21@gmail.com>
2022-12-15 10:39:44 -0300

    Function done.

--- IntermediateChallenges_5kyu/pig_it.py
+++ IntermediateChallenges_5kyu/pig_it.py
@@ -4,7 +4,7 @@ print()
 def pig_it(string):
 
     # This function moves the first letter of each word to the end of it,
-    # then add "ay" to the end of the word. Leave punctuation marks untouched.
+    # then adds "ay" to the end of the word. Leave punctuation marks untouched.
     return ' '.join([w[1:] + w[0] + 'ay' if w.isalpha() else w for w in string.split()])
 
 print(pig_it('Pig latin is cool !')) # Output - igPay atinlay siay oolcay ! 
