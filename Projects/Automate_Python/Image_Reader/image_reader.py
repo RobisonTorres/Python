9a0fad69f1d9a2507eabcb44592a39bc98fc2ba4
RobisonTorres <robison.torres21@gmail.com>
2022-12-23 13:32:10 -0300

    function done.

--- Projects/Automate_Python/Image_Reader/image_reader.py
+++ Projects/Automate_Python/Image_Reader/image_reader.py
@@ -2,9 +2,9 @@ from PIL import Image
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-image_path = r'C:\Users\user\Python\Info.jpg'
+# image_path = r'C:\Users\user\Python\Info.jpg'
 
-with open(image_path, 'rb') as file:
+with open("Info.jpg", 'rb') as file:
     text = pytesseract.image_to_string(Image.open(file))
 
 print(text)
