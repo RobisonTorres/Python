773715541a75e5a0b1eb82b5697d433398f04df1
RobisonTorres <robison.torres21@gmail.com>
2022-12-28 14:29:25 -0300

    working.

--- test_free.py
+++ test_free.py
@@ -5,22 +5,3 @@ print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
 
-def fibonacci(n):
-    if n <= 1:  # base case
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-def test(number):
-
-    fn_number = 0
-    n = 0
-    while fn_number < number:
-        fn_number = fibonacci(n) * fibonacci(n+1)
-        n += 1
-    if fn_number == number:
-        return [fibonacci(n-1), fibonacci(n), True]
-    else:
-        return [fibonacci(n-1), fibonacci(n), False]
-
-print(test(4895))
