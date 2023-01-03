097d6cbfb50d9e2fef134c9f1ef1c464cabd3d09
RobisonTorres <robison.torres21@gmail.com>
2023-01-03 10:23:49 -0300

    initial commit.

---
+++ IntermediateChallenges_5kyu/scramble.py
@@ -0,0 +1,8 @@
+print('From Code Wars.')
+print()
+
+def scramble(s1, s2):
+
+    # This function...
+    pass
+
--- test_free.py
+++ test_free.py
@@ -5,3 +5,8 @@ print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
 
+def scramble(s1, s2):
+
+    return set(s2).issubset(set(s1))
+
+print(scramble("scriptjavx", "javascript"))
