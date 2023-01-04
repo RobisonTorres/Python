037a7e4ac4f48fc1a2d1d2b5098f5e3a7e80069f
RobisonTorres <robison.torres21@gmail.com>
2023-01-04 09:40:21 -0300

    function done.

--- IntermediateChallenges_5kyu/scramble.py
+++ IntermediateChallenges_5kyu/scramble.py
@@ -1,8 +1,11 @@
+from collections import Counter
 print('From Code Wars.')
 print()
 
 def scramble(s1, s2):
+   
+    # This fuction checks if a portion of str1 characters 
+    # can be rearranged to match str2.
+    return Counter(s1) >= Counter(s2)
 
-    # This function...
-    pass
-
+print(scramble("camtjtfhooex", "hotjotac"))  # True.
--- test_free.py
+++ test_free.py
@@ -5,8 +5,3 @@ print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
 
-def scramble(s1, s2):
-
-    return set(s2).issubset(set(s1))
-
-print(scramble("scriptjavx", "javascript"))
