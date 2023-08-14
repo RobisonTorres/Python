d00b9312d2af9417e6c6f593d33bce49709d4892
RobisonTorres <robison.torres21@gmail.com>
2023-08-14 17:44:28 -0300

    code update.

--- BasicChallenges_7kyu/lottery.py
+++ BasicChallenges_7kyu/lottery.py
@@ -1,5 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
 print('Checking number...')
 print()
+
+game = input('Please, type in the number of the round ')
+print()
+source = requests.get('https://www.google.com/search?q=lotofacil+'+game).text
+soup = BeautifulSoup(source, 'html.parser')
+numbers_result = soup.find('span', class_="FCUp0c rQMQod").text.replace('-','')
+print(numbers_result)
 
 def score(lottery_numbers, your_games):
 
@@ -10,9 +19,9 @@ def score(lottery_numbers, your_games):
         result.append(str(num) + '° Game - Score: ' 
                                +  str(15 - len(set(lottery_numbers) - set(game))))
     return '\n'.join(result)
-    
-print(score('', 
-            [[],
+
+print(score(numbers_result, 
+            [[1,3,5,6,8,10,11,13,15,16,17,19,20,21,24],
             [],
             [],
             [],
