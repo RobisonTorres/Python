e779d19955ca4844b973def26587aba9370b7a7d
RobisonTorres <robison.torres21@gmail.com>
2023-08-17 20:05:41 -0300

    first selenium commit.

--- test_free.py
+++ test_free.py
@@ -1,9 +1,13 @@
+from selenium import webdriver
 import sys, subprocess
 subprocess.run('cls', shell=True)
 print('Test Free')
 print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
+
+path = r"C:\Users\rcssi\Downloads\geckodriver"
+drive = webdriver.Firefox
 
 print('testing')
 
