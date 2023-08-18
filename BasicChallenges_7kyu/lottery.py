1a93fffebc4e3d310443263a36b51d169296b058
RobisonTorres <robison.torres21@gmail.com>
2023-08-18 20:27:50 -0300

    working web scraping.

--- BasicChallenges_7kyu/lottery.py
+++ BasicChallenges_7kyu/lottery.py
@@ -1,4 +1,19 @@
-print('Checking number...')
+from bs4 import BeautifulSoup
+from googlesearch import search
+import requests
+import re
+print('Checking the numbers...')
+print()
+
+query = f"lotofacil + {input('round: ')} + uol.com.br"
+url = search(query, tld="co.in", num=1, stop=1, pause=2)
+
+source = requests.get(*url).text
+
+soup = BeautifulSoup(source, 'html.parser')
+
+numbers = re.findall(r'(?:(?:[0-9]{2}-?){15})', soup.text)[0].replace('-','')
+print(numbers)
 print()
 
 def score(lottery_numbers, your_games):
@@ -11,8 +26,8 @@ def score(lottery_numbers, your_games):
                                +  str(15 - len(set(lottery_numbers) - set(game))))
     return '\n'.join(result)
 
-print(score('', 
-            [[],
+print(score(numbers, 
+            [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
             [],
             [],
             [],
--- test_free.py
+++ test_free.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 import sys, subprocess
 subprocess.run('cls', shell=True)
 print('Test Free')
@@ -6,8 +5,4 @@ print()
 # crt + D / alt + mouse / alt + up or down / shift + alt + up or down / crt + l
 # crt + N .. crt + k, m .. crt + s
 
-#path = r"C:\Users\rcssi\Downloads\geckodriver"
-#drive = webdriver.Firefox
-
-driver = webdriver.Firefox()
-driver.get("https://www.youtube.com/")
+print('Testing')
