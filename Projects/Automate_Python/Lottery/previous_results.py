456cf8691932c43360da5a1a2db51011b40dcbb3
RobisonTorres <robison.torres21@gmail.com>
2023-08-21 20:12:02 -0300

    final code.

--- Projects/Automate_Python/Lottery/previous_results.py
+++ Projects/Automate_Python/Lottery/previous_results.py
@@ -2,31 +2,41 @@ import googlesearch
 import bs4
 import requests
 import re
-print('Checking previous results...')
+print('Checking Previous Results.')
 print()
 
 def score():
     
-    # This function checks five last results.
+    # This function returns ten previous results from the lottery - 'lotofacil'.
     round = int(input('Type in the last lottery round: '))
     print()
-    results = []
+    results= []
 
-    for num in range((round-4), round+1):
+    for num in range((round-9), round+1):
 
         query = f"lotofacil + {num} + noticias.uol.com.br"
         url = googlesearch.search(query, tld="co.in", num=1, stop=1, pause=2)
-
         source = requests.get(*url).text
         soup = bs4.BeautifulSoup(source, 'html.parser')
 
         # Extracting the lottery result from the scraping.
         lottery_result = re.findall(r'(?:(?:[0-9]{2}-?){15})', soup.text)[0]
+        print(f'Round: {num} - Result: {[lottery_result]}')
 
         # Storing the results.
-        results.append(f'Round: {num} - Result: {[lottery_result]}')
+        results.append(lottery_result)
 
-    return '\n\n'.join(results)
+    print()
+
+    # Getting the frequency of each number.
+    frequency = []
+    results = ''.join(results).replace('-', '')  
+    lottery_result = [int(results[x: x+2]) for x in range(0, len(results), 2)]
+
+    for num in range(1, 26):
+        frequency.append(f'num {num}: {lottery_result.count(num)}')
+
+    print('Frequency')    
+    return frequency
 
 print(score())
-print()
