7e6990af2d8566d54547e5f3bfeb06188fdcc1f3
RobisonTorres <robison.torres21@gmail.com>
2023-08-23 19:42:07 -0300

    updates.

--- Projects/Automate_Python/Lottery/previous_results.py
+++ Projects/Automate_Python/Lottery/previous_results.py
@@ -2,51 +2,47 @@ import googlesearch
 import bs4
 import requests
 import re
+import json
+
 print('Checking Previous Results.')
 print()
 
 def score():
+
+    # This function retrieves twenty previous results from the 'lotofacil' lottery, 
+    # either from a local JSON file or through web scraping.
+    # It also stores the results in a JSON file for future use.
     
-    # This function returns twenty previous results from the 'lotofacil' lottery, 
-    # the frequency of each number, and the five numbers most drew.
+    file = open('pre_results.json')
     round = int(input('Type in the last lottery round: '))
     print()
-    results= []
+    results = json.load(file)
+    
+    for num in range((round - 19), round + 1):
+        if str(num) in results.keys():
 
-    for num in range((round-19), round+1):
+            # If the result for this round is already in the saved results, display it.
+            print(f'Round: {num} - Result: {results[str(num)]}')
 
-        # Taking the lottery result through web scraping.
-        query = f"lotofacil + {num} + noticias.uol.com.br"
-        url = googlesearch.search(query, tld="co.in", num=1, stop=1, pause=2)
-        source = requests.get(*url).text
-        soup = bs4.BeautifulSoup(source, 'html.parser')
+        else:
 
-        # Extracting only the numbers.
-        lottery_result = re.findall(r'(?:(?:[0-9]{2}\s?-?\s?){15})', soup.text)[0].replace(' ', '')  
-        print(f'Round: {num} - Result: {[lottery_result]}')
-
-        # Storing the results.
-        results.append(lottery_result)
-    print()
-
-    # Getting the frequency of each number.
-    frequency = {}
-    results = ''.join(results).replace('-', '') 
-    lottery_result = [int(results[x: x+2]) for x in range(0, len(results), 2)]
-
-    for num in range(1, 26):
-        frequency[num] = lottery_result.count(num)
-
-    print('Frequency of each number.')    
-    print(frequency)
-    print()
-
-    # Numbers to consider based on previous results.
-    frequency = sorted(frequency.items(),key=lambda x:x[1])[::-1]
-    numbers = list(dict(frequency).keys())
-    print('Numbers most drew in recent rounds.')
-    return sorted(numbers[0:5])
+            # If the result is not saved, retrieve it through web scraping.
+            query = f"lotofacil + {num} + noticias.uol.com.br"
+            url = googlesearch.search(query, tld="co.in", num=1, stop=1, pause=2)
+            source = requests.get(*url).text
+            soup = bs4.BeautifulSoup(source, 'html.parser')
+            
+            # Extract only the numbers from the scraped text.
+            lottery_result = re.findall(r'(?:(?:[0-9]{2}\s?-?\s?){15})', soup.text)[0].replace(' ', '')
+            print(f'Round: {num} - Result: {lottery_result}')
+            
+            # Store the result in the 'results' dictionary.
+            results[num] = lottery_result
+    
+    # Save the updated results dictionary back to the JSON file.
+    with open('pre_results.json', 'w') as file:
+        json.dump(results, file)
+            
+    return '\nPrevious Results Saved.'
 
 print(score())
-
-# It's important to remember that past outcomes do not guarantee future results in a lottery.
