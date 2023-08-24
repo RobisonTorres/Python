ed3ef412e0352a60d8ede9dd5db460c9d8f98b56
RobisonTorres <robison.torres21@gmail.com>
2023-08-24 16:37:32 -0300

    update.

--- Projects/Automate_Python/Lottery/checking_result.py
+++ Projects/Automate_Python/Lottery/checking_result.py
@@ -2,15 +2,14 @@ import googlesearch
 import requests
 import bs4
 import re
-print('Checking numbers...')
+print('Checking numbers.')
 print()
 
 def score(your_games):
     
     # This function checks the total score hit in each lottery ticket.
     round = input('What round do you want to check: ')
-    print()
-
+    
     # Taking the lottery result through web scraping.
     query = f"lotofacil {round} noticias.uol.com.br"
     url = googlesearch.search(query, tld="co.in", num=1, stop=1, pause=2)
@@ -19,9 +18,9 @@ def score(your_games):
 
     # Extracting only the lottery result from the scraping,
     # and giving it a proper format.
-    lottery_result = re.findall(r'(?:(?:[0-9]{2}\s?-?\s?){15})', soup.text)[0].replace(' ', '')
-    print(f'Result - {round} : {lottery_result}')
-    print()
+    lottery_result = re.findall(r'(?:(?:[0-9]{2}\s?\.?-?\s?){14}(?:[0-9]{2}){1})', soup.text)[0]
+    lottery_result = lottery_result.replace(' ', '').replace('.', '-')
+    print(f'\nResult - {round} : {lottery_result}\n')
     lottery_result = lottery_result.replace('-','')
     lottery_result = [int(lottery_result[x: x+2]) for x in range(0, len(lottery_result), 2)]   
 
@@ -33,12 +32,6 @@ def score(your_games):
     return '\n'.join(result)
 
 print(score([[2,4,8,9,10,11,12,14,17,18,19,20,21,22,25],
-            [2,4,6,7,8,10,12,14,15,16,18,20,23,24,25],
-            [1,4,6,7,9,10,11,12,15,16,18,19,20,21,23],
-            [],
-            [],
-            [],
-            [],
             [],
             [],
             [],
--- Projects/Automate_Python/Lottery/previous_results.py
+++ Projects/Automate_Python/Lottery/previous_results.py
@@ -4,7 +4,7 @@ import requests
 import re
 import json
 
-print('Checking Previous Results.')
+print('Gathering Previous Results.')
 print()
 
 def score():
@@ -19,29 +19,28 @@ def score():
     
     for num in range((round - 19), round + 1):
         if str(num) in results.keys():
-
             # If the result for this round is already in the saved results, display it.
             print(f'Round: {num} - Result: {results[str(num)]}')
 
         else:
-
             # If the result is not saved, retrieve it through web scraping.
             query = f"lotofacil + {num} + noticias.uol.com.br"
             url = googlesearch.search(query, tld="co.in", num=1, stop=1, pause=2)
             source = requests.get(*url).text
             soup = bs4.BeautifulSoup(source, 'html.parser')
             
-            # Extract only the numbers from the scraped text.
-            lottery_result = re.findall(r'(?:(?:[0-9]{2}\s?-?\s?){15})', soup.text)[0].replace(' ', '')
+            # Extracting only the numbers from the scraped text.
+            lottery_result = re.findall(r'(?:(?:[0-9]{2}\s?\.?-?\s?){14}(?:[0-9]{2}){1})', soup.text)[0]
+            lottery_result = lottery_result.replace(' ', '').replace('.', '-')
             print(f'Round: {num} - Result: {lottery_result}')
             
             # Store the result in the 'results' dictionary.
             results[num] = lottery_result
     
-    # Save the updated results dictionary back to the JSON file.
+    # Save the updated results dictionary back to the JSON file for future use.
     with open('pre_results.json', 'w') as file:
         json.dump(results, file)
             
-    return '\nResults Saved.'
+    return '\nResults have been saved.'
 
 print(score())
