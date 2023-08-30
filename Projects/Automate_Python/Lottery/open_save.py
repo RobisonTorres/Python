bcbce19ac68c2521da7759866b087b15cbf7ad5a
RobisonTorres <robison.torres21@gmail.com>
2023-08-30 18:10:39 -0300

    first commit.

---
+++ Projects/Automate_Python/Lottery/open_save.py
@@ -0,0 +1,18 @@
+import json
+
+def open_file():
+     
+     # This function opens and loads json files.
+     try:
+        file = open('pre_results.json')
+        previous_result = json.load(file)
+        return previous_result
+     except:
+         return 'File not found.'
+
+def save_file(x):
+    
+    # Save the updated results.
+    with open('pre_results.json', 'w') as file:
+        json.dump(x, file, indent = 1)
+
