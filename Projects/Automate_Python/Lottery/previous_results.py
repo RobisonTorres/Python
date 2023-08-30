f1929243ed8ef771586f9c2768008af70a969799
RobisonTorres <robison.torres21@gmail.com>
2023-08-30 18:11:49 -0300

    update.

--- Projects/Automate_Python/Lottery/previous_results.py
+++ Projects/Automate_Python/Lottery/previous_results.py
@@ -1,30 +1,28 @@
+import open_save
 import googlesearch
 import bs4
 import requests
 import re
-import json
 
 print('Gathering Previous Results.')
 print()
 
 def score():
 
-    # This function retrieves twenty previous results from the 'lotofacil' lottery, 
-    # through web scraping or local file, and saves the new result for future use.
+    # This function retrieves twenty previous results from the lottery, 
+    # through web scraping or local file, and saves new results for future use.
 
-    # Open file.
-    file = open('pre_results.json')
+    previous_results = open_save.open_file()
     round = int(input('Type in the last lottery round: '))
-    print()
-    results = json.load(file)
-    results = dict(sorted(results.items(),key=lambda x:x[0]))
     
     for num in range((round - 19), round + 1):
-        if str(num) in results.keys():
+        if str(num) in previous_results.keys():
+
             # If the result for this round is already in the saved results, display it.
-            print(f'Round: {num} - Result: {results[str(num)]}')
+            print(f'Round: {num} - Result: {previous_results[str(num)]}')
 
         else:
+
             # If the result is not saved, retrieve it through web scraping.
             query = f"lotofacil + {num} + noticias.uol.com.br"
             url = googlesearch.search(query, tld="co.in", num=1, stop=1, pause=2)
@@ -36,13 +34,14 @@ def score():
             lottery_result = lottery_result.replace(' ', '').replace('.', '-')
             print(f'Round: {num} - Result: {lottery_result}')
             
-            # Store the result in the 'results' dictionary.
-            results[num] = lottery_result
-    
-            # Save the updated results.
-            with open('pre_results.json', 'w') as file:
-                json.dump(results, file, indent = 1)
-            
+            # Save new results.
+            previous_results[num] = lottery_result
+            open_save.save_file(previous_results)
+
+    # Sorted the dictionary and save.
+    previous_results = dict(sorted(previous_results.items(),key=lambda x:int(x[0])))
+    open_save.save_file(previous_results)        
+
     return '\nNews results have been saved.'
 
 print(score())
