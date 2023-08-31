75040cdf7292a090e4a781a0a6d65263a526e7d3
RobisonTorres <robison.torres21@gmail.com>
2023-08-31 15:19:01 -0300

    code done.

--- Projects/Automate_Python/Lottery/web_scraper.py
+++ Projects/Automate_Python/Lottery/web_scraper.py
@@ -1,4 +1,3 @@
-import googlesearch
 import requests
 import bs4
 import re
@@ -6,28 +5,34 @@ import open_save
 
 def web_search(round):
 
-    # This function...
-    query = f"lotofacil {round} noticias.uol.com.br"
-    url = googlesearch.search(query, tld="co.in", num=1, stop=1, pause=2)
-    source = requests.get(*url).text
+    # This function accesses the lottery page and takes the result.
+    url = f'https://www.intersena.com.br/lotofacil/resultados/{round}'
+    source = requests.get(url).text
     soup = bs4.BeautifulSoup(source, 'html.parser')
     return soup
 
 def extract_search(round=False):
 
-    # This function...
+    # This function extracts only the lottery result from the scraping,
+    # gives it a proper format, and saves new results.
     previous_results = open_save.open_file()
-    if not round:
-         round = input('What round do you want to check: ')
     
-    target = web_search(round)
+    if not round:
+        round = input('What round do you want to check: ')
 
-    # Extracting only the lottery result from the scraping,
-    # and giving it a proper format.
-    lottery_result = re.findall(r'(?:(?:[0-9]{2}\s?\.?-?\s?){14}(?:[0-9]{2}){1})', target.text)[0]
-    lottery_result = lottery_result.replace(' ', '').replace('.', '-')
+    try:
+        target = web_search(round)
+        numbers = target.find('div', class_='resultado-individual-sorteio')
 
-    previous_results[round] = lottery_result
-    previous_results = dict(sorted(previous_results.items(),key=lambda x:int(x[0])))
-    open_save.save_file(previous_results)   
-    return [round ,lottery_result]
+        # Only numbers.
+        nums = re.sub(r'[\n\s]', '', numbers.text)
+        lottery_result = '-'.join([nums[x:x+2] for x in range(0, len(nums), 2)])
+
+        # Save.
+        previous_results[round] = lottery_result
+        previous_results = dict(sorted(previous_results.items(),key=lambda x:int(x[0])))
+        open_save.save_file(previous_results)   
+        return [round ,lottery_result]
+
+    except:
+        return 'Something went wrong, please try again.'
