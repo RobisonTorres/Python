fd011f4bcd458bbe6bfb4863ee5db48a4581787e
RobisonTorres <robison.torres21@gmail.com>
2023-09-05 14:53:03 -0300

    first commit.

---
+++ Projects/Automate_Python/Lottery/autocheck_score.py
@@ -0,0 +1,32 @@
+import checking_score
+import sys
+sys.path.append(r'C:\Users\rcssi\Code\Python\Python\Projects\Automate_Python\Image_Reader')
+from reader_numbers import read_nums
+
+def get_numbers():
+
+    # This function retrieves numbers from image and processes them to form
+    # lottery games. Then it gives the total score hit with each game.
+    your_games = []   
+    numbers_retrieve = read_nums(f'games1.png')
+    games_retrieve =  numbers_retrieve.replace('#', ' ').split()
+    
+    for x in range(0, len(games_retrieve)):
+        nums = [n for n in games_retrieve[x]][1:]  # The first number is the n° of the game.   
+        numbers = [0]
+
+        while len(numbers) <= 15:
+            if int(nums[0]) > max(numbers):
+                numbers.append(int(nums[0]))
+                nums = nums[1:]
+            else:
+                numbers.append(int(''.join(nums[0:2])))            
+                nums = nums[2:]
+
+        your_games.append(numbers[1:])
+        print(f'{x + 1}° Game: {"-".join([str(n).zfill(2) for n in numbers[1:]])}')
+    print()
+    result = checking_score.score(your_games)
+    return result
+
+print(get_numbers())
