b19efe893b4578be9692f73a80e5f7cef730178d
RobisonTorres <robison.torres21@gmail.com>
2023-09-06 22:53:20 -0300

    code updated.

--- Projects/Automate_Python/Lottery_Score/autocheck_score.py
+++ Projects/Automate_Python/Lottery_Score/autocheck_score.py
@@ -1,28 +1,22 @@
 import sys
 sys.path.append(r'C:\Users\rcssi\Code\Python\Python\Projects\Automate_Python\Image_Reader')
-from reader_numbers import read_nums
-from checking_score import score
+import reader_numbers, checking_score
 
 def get_numbers():
 
     # This function retrieves numbers from image and processes them to form lottery games. 
     your_games = []   
-    numbers_retrieve = read_nums(f'games.png')
+    numbers_retrieve = reader_numbers.read_nums(f'games.png')
     games_retrieve =  numbers_retrieve.replace('#', ' ').split()
     
     for x in range(0, len(games_retrieve)):
-        nums = [n for n in games_retrieve[x]][1:]  # The first number is the n° of the game.   
-        numbers = [int(nums.pop(0))]
+        nums = [int(n) for n in games_retrieve[x]][1:]  # The first number is the n° of the game.   
+        numbers = [nums.pop(0)]
 
         while len(numbers) < 15:
-            if int(nums[0]) > max(numbers):
-                numbers.append(int(nums[0]))
-                nums = nums[1:]
-            else:
-                numbers.append(int(''.join(nums[0:2])))            
-                nums = nums[2:]
+            numbers.append(nums.pop(0) if (nums[0]) > max(numbers) 
+                                       else int(str(nums.pop(0)) + str(nums.pop(0))))
         your_games.append(numbers)
-
     return your_games
 
 def auto_check():
@@ -32,8 +26,8 @@ def auto_check():
     for n, game in enumerate(your_games, 1):
         print(f'{str(n)} ° Game - {("-".join(map(str, game)))}')
     print()
-
-    lottery_result = score(your_games)
+    
+    lottery_result = checking_score.score(your_games)
     return lottery_result
 
 print(auto_check())
