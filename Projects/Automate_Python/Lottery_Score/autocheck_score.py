5de0899c1312881a894d4513c8e243a489bdc323
RobisonTorres <robison.torres21@gmail.com>
2023-09-10 11:36:14 -0300

    code updated.

--- Projects/Automate_Python/Lottery_Score/autocheck_score.py
+++ Projects/Automate_Python/Lottery_Score/autocheck_score.py
@@ -6,27 +6,33 @@ def get_numbers():
 
     # This function retrieves numbers from image and processes them to form lottery games. 
     your_games = []   
-    numbers_retrieve = reader_numbers.read_nums(f'games.png')
-    games_retrieve =  re.sub(r'(#\d)', ' ', numbers_retrieve).split() # Remove '#' and the first digit.
-    
-    for x in range(0, len(games_retrieve)):
-        nums = [int(n) for n in games_retrieve[x]]   
+    numbers_retrieved = reader_numbers.read_nums(f'games3.png')
+    round_retrieved = re.sub(r'(#\d+)|(\@)', '', numbers_retrieved)
+    games_retrieved =  re.sub(r'(#\d)|(\@\d+)', ' ', numbers_retrieved).split()
+   
+    for x in range(0, len(games_retrieved)):
+        nums = [int(n) for n in games_retrieved[x]]   
         numbers = [nums.pop(0)]
 
         while len(numbers) < 15:
             numbers.append(nums.pop(0) if (nums[0]) > numbers[-1]
                                        else int(str(nums.pop(0)) + str(nums.pop(0))))
-        your_games.append(numbers)
-    return your_games
+        your_games.append(numbers)   
+
+    return [your_games, round_retrieved]
 
 def auto_check():
 
     # This function checks the total score hit in each game.
-    your_games = get_numbers()
+    image_result = get_numbers()
+    your_games = image_result[0]
+    round = image_result[1]
+
     for n, game in enumerate(your_games, 1):
         print(f'{str(n)}° Game - {("-".join(map(str, game)))}')
     print()   
-    lottery_result = checking_score.score(your_games)
+
+    lottery_result = checking_score.score(your_games, round)
     return lottery_result
 
 print(auto_check())
--- Projects/Automate_Python/Lottery_Score/web_scraper.py
+++ Projects/Automate_Python/Lottery_Score/web_scraper.py
@@ -8,11 +8,11 @@ def web_search(round):
     soup = bs4.BeautifulSoup(source, 'html.parser')
     return soup
 
-def extract_search():
+def extract_search(round=False):
 
     # This function extracts only the lottery result from 
     # the web_search, and gives a proper format to the result.
-    round = input('What round do you want to check: ')
+    if round == False: round = input('What round do you want to check: ')
     
     target = web_search(round)
     round_found = target.find('h1', class_='col-md-6 col-sm-6 col-xs-12')  
@@ -22,7 +22,7 @@ def extract_search():
     round_found = re.sub('[^0-9]', '', round_found.text)
     lottery_numbers = re.sub('[^0-9]', '', lottery_numbers.text)
       
-    if round != round_found: 
+    if str(round) != round_found: 
         return f'\nRound: {round} - Result not found.\n'
 
     else:
