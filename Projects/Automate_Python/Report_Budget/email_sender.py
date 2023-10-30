baa448e8285df226a621e5c3e0ec39463a2428e9
RobisonTorres <robison.torres21@gmail.com>
2023-10-30 15:26:54 -0300

    work done.

--- Projects/Automate_Python/Report_Budget/email_sender.py
+++ Projects/Automate_Python/Report_Budget/email_sender.py
@@ -1,25 +1,29 @@
-import take_report
-import win32com.client as win32
-import os
+import take_report, win32com.client as win32, os
 print('Sending Email.')
 
-# Python-Outlook integration.
-outlook = win32.Dispatch('outlook.application')
+outlook = win32.Dispatch('outlook.application') # Python-Outlook integration.
 mail = outlook.CreateItem(0)
 
 def send_email():
 
-    # This function sends the email with the monthly Personal Budget Report.
-    take_report.hello()
+    # This function sends the email with the monthly Personal Budget.
+    balance = take_report.get_balance()
     mail.to = input('Please type in the recipient: ')
     mail.Subject = 'Monthly Personal Budget Report.'
-    mail.HTMLBody = r'''
+    email_content = f'''
 
-    Type here any message that you want.
+    Dear Mr. ,
+
+    I hope this message finds you well.
+    {balance}
+    Attached, please find the detailed budget. This report encompasses a comprehensive \n
+    overview of our financial activities, including income, expenditures, savings, and \n
+    any other pertinent financial updates.
 
     '''
+    mail.HTMLBody = email_content
     try:
-        mail.Attachments.Add(os.path.join(os.getcwd(), "Budget_example.xlsx"))  # Or P_Budget.xlsx
+        mail.Attachments.Add(os.path.join(os.getcwd(), "Personal_Budget.xlsx"))
         mail.Send()        
         return '\nThe email has sent with success.'
     except:
--- Projects/Automate_Python/Report_Budget/take_report.py
+++ Projects/Automate_Python/Report_Budget/take_report.py
@@ -1,6 +1,14 @@
+import openpyxl, warnings
 
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category = UserWarning)
+    wb = openpyxl.load_workbook('Personal_Budget.xlsx', data_only = True)
+    wb = wb['Balance & Tracking']
 
+def get_balance():
 
-def hello():
-    return 'hello'
-
+    # This function extract data from Personal_Budget.xlsx.
+    annual_balance = wb['E4'].value
+    current_balance = wb['E5'].value
+    return f'The current balance son far is $ {current_balance}.'\
+           f'The annual balance is $ {annual_balance}.' 
