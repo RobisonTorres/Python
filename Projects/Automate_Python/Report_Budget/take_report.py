a8bc12eca42d7470ced95a312221a1a3e41ea8b7
RobisonTorres <robison.torres21@gmail.com>
2023-10-30 15:31:37 -0300

    work done.

--- Projects/Automate_Python/Report_Budget/take_report.py
+++ Projects/Automate_Python/Report_Budget/take_report.py
@@ -10,5 +10,5 @@ def get_balance():
     # This function extract data from Personal_Budget.xlsx.
     annual_balance = wb['E4'].value
     current_balance = wb['E5'].value
-    return f'The current balance son far is $ {current_balance}.'\
+    return f'The current balance is $ {current_balance}.'\
            f'The annual balance is $ {annual_balance}.' 
