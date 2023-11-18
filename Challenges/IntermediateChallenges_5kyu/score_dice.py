58279b4faec4194abc057d6266cc26d03b1672e4
RobisonTorres <robison.torres21@gmail.com>
2023-11-17 23:11:29 -0300

    code updated.

--- Challenges/IntermediateChallenges_5kyu/score_dice.py
+++ Challenges/IntermediateChallenges_5kyu/score_dice.py
@@ -4,31 +4,24 @@ print()
 def score(dice):
 
     # This function...
-    dice = ''.join(sorted(map(str, dice)))
+    throws = ''.join(sorted(map(str, dice)))
     points = 0
     scores = {'111': 1000, '666': 600, '555': 500, '444': 400,
              '333': 300, '222': 200, '1': 100, '5': 50}
 
-    for n in range(1, 7):
-        if str(n) in dice:
-            if str(n) == '1' or str(n) == '5':
-                numbers = ''.join([m for m in dice if m == str(n)])
-                if len(numbers) == 5:
+    for n in list(set(dice)):
+        numbers = ''.join([m for m in throws if m == str(n)])
+        while numbers:
+            if len(numbers) >= 3:
+                if numbers[:3] in scores:
                     points += scores[numbers[:3]]
-                    points += scores[numbers[3]]
-                    points += scores[numbers[4]]
-                elif len(numbers) == 4:
-                    points += scores[numbers[:3]]
-                    points += scores[numbers[3]]
-                elif len(numbers) == 3 or len(numbers) == 1:
-                    points += scores[numbers]
-                elif len(numbers) == 2:
-                    points += scores[numbers[0]]
-                    points += scores[numbers[1]]
+                    numbers = numbers[3:]
             else:
-                numbers = ''.join([m for m in dice if m == str(n)][:3])
-                if numbers in scores:
-                    points += scores[numbers]
+                if numbers[:1] in scores:
+                    points += scores[numbers[0]]
+                    numbers = numbers[1:]
+                else:
+                    numbers = numbers[1:]
     return points
 
 print(score([5, 1, 3, 4, 1]))
