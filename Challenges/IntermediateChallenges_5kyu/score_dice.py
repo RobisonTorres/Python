20a7a6f3d204eaf8af657cdd36faf9e20957c5f6
RobisonTorres <robison.torres21@gmail.com>
2023-11-18 10:15:00 -0300

    final updated.

--- Challenges/IntermediateChallenges_5kyu/score_dice.py
+++ Challenges/IntermediateChallenges_5kyu/score_dice.py
@@ -3,24 +3,20 @@ print()
 
 def score(dice):
 
-    # This function...
+    # This function calculates the total score of throws in the Greed dice game.
     throws = ''.join(sorted(map(str, dice)))
     points = 0
-    scores = {'111': 1000, '666': 600, '555': 500, '444': 400,
-             '333': 300, '222': 200, '1': 100, '5': 50}
-
+    scores = {'111': 1000, '666': 600, '555': 500, '444': 400, 
+              '333': 300, '222': 200, '1': 100, '5': 50}
+    
     for n in list(set(dice)):
         numbers = ''.join([m for m in throws if m == str(n)])
         while numbers:
-            if numbers[:3] in scores:
-                points += scores[numbers[:3]]
-                numbers = numbers[3:]
-            elif numbers[:1] in scores:
-                points += scores[numbers[0]]
-                numbers = numbers[1:]
-            else:
-                numbers = numbers[1:]
+            for n in [3, 1, 1]:
+                if numbers[:n] in scores:
+                    points += scores[numbers[:n]]
+                    numbers = numbers[n:]
+            numbers = numbers[1:]            
     return points
 
-print(score([5, 1, 3, 4, 1]))
-print(score([1, 1, 1, 3, 1]))
+print(score([5, 1, 3, 4, 1]))  # Outputs - 250 points
